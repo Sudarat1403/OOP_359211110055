59c800de8a8134bf71dd8a1599852b1c9d67d16b
sudarat1403 <sudaratraprit.com02@gmail.com>
2018-01-31 11:26:07 +0700

    EmployeeApp

--- src/ooplab8/EmployeeApp.java
+++ src/ooplab8/EmployeeApp.java
@@ -4,7 +4,7 @@ public class EmployeeApp {
 
     public static void main(String[] args) {
 
-        Employee employee = new Employee("mook", "male","20/12/1990",20, "Secretary", "500,000");
+        Employee employee = new Employee("Far", "male","14/03/1995",23, "Secretary", "35000");
 
         System.out.print(employee.toString());
     }//main
