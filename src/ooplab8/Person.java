7f21e9d346921c93a9c22736769eeb241586b7c5
sudarat1403 <sudaratraprit.com02@gmail.com>
2018-01-31 09:13:23 +0700

    Person

---
+++ src/ooplab8/Person.java
@@ -0,0 +1,63 @@
+package ooplab8;
+
+public class Person {
+    private String name;
+    private String gander;
+    private String dateOfBirth;
+    private String age;
+    //constructor
+
+
+    public Person(String name, String gander, String dateOfBirth, String age) {
+        this.name = name;
+        this.gander = gander;
+        this.dateOfBirth = dateOfBirth;
+        this.age = age;
+    }
+    //toString()
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", gander='" + gander + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGander() {
+        return gander;
+    }
+
+    public void setGander(String gander) {
+        this.gander = gander;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}//class
