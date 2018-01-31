1998f45d3929a22bdd332ec2f7356573d37b8082
sudarat1403 <sudaratraprit.com02@gmail.com>
2018-01-31 11:21:01 +0700

    PetApp

--- src/ooplab8/PetApp.java
+++ src/ooplab8/PetApp.java
@@ -2,6 +2,7 @@ package ooplab8;
 
 public class PetApp {
 
+
     public static void main(String[] args) {
         Dog dog = new Dog("Chayen",2,"Bangkeaw");
         System.out.println(dog.toString());
@@ -10,6 +11,8 @@ public class PetApp {
         Cat cat = new Cat("Giant",2,"persian");
         System.out.println(cat.toString());
         cat.makeNoise();
+
+
 
     }//main
 }//class
