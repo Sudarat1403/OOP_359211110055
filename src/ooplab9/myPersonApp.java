4aaaaf3b06ecc7666d540fbbf43a94855d30fe7f
sudarat1403 <sudaratraprit.com02@gmail.com>
2018-02-21 09:22:20 +0700

    Address

---
+++ src/ooplab9/myPersonApp.java
@@ -0,0 +1,22 @@
+package ooplab9;
+
+public class myPersonApp {
+
+    public static void main(String[] args) {
+        Person person = new Person(
+                "1900100111351",
+                "Natpatcha Phetrat",
+                new Address("34/7 M.5",
+                        "Nakornsithammarat",
+                        "80140"),
+                new Job("manager",
+                        50000));
+
+        System.out.println(person.toString());
+
+        person.getJob().setSalary(50000);
+
+        System.out.println(person.getJob());
+
+    }
+}//class
